e76a1bc61ef65ad4dc42dacf1899728767fdcba9
trung <trungtech1908@gmail.com>
2026-05-18 09:04:38 +0700

    Update

--- assignments/Phase_1/Bai7.py
+++ assignments/Phase_1/Bai7.py
@@ -4,14 +4,19 @@ from Bai1 import WhitespaceTokenizer
 
 
 class IDF:
-    def __init__(self):
+    def __init__(self, smooth=False):
+        self.smooth = smooth
         self.tokenizer = WhitespaceTokenizer()
 
     def compute(self, token, documents):
         token = token.lower()
         tokens_list = [self.tokenizer.tokenize(document) for document in documents]
         df = sum(1 for tokens in tokens_list if token in tokens)
-        return math.log(len(documents) / df)
+        N = len(documents)
+
+        if self.smooth:
+            return math.log((N + 1) / (df + 1)) + 1
+        return math.log(N / df) if df > 0 else 0.0
 
 
 idf = IDF()
